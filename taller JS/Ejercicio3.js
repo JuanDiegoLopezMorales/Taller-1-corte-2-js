7a6666a7122021641da50340db65eaa41fbbeae0
JuanDiegoLopezMorales <lopezjuandiego411@gmail.com>
2024-09-16 16:43:45 -0500

    Update Ejercicio3.js

--- taller JS/Ejercicio3.js
+++ taller JS/Ejercicio3.js
@@ -1,22 +1,5 @@
-const sumaDigitos = num => {
-    // Convertir el número a una cadena de texto
-    const numString = Math.abs(num).toString();
-
-// Inicializar la variable para almacenar la suma
-let suma = 0
-
-// Iterar sobre cada digito de la cadena de texto
-for (let i = 0; i < numString.length; i++){
-    // Convertir el dígito actual de cadena a número
-    const digito = parseInt(numString[i]);
-
-    // Sumar el dígito a la variable suma
-    suma += digito;
+function sumar(num1, num2) {
+    return num1 + num2;
 }
 
-// Devolver la suma de los digitos
-return suma;
-}
-console.log(sumaDigitos(12345));
-console.log(sumaDigitos(-67));
-console.log(sumaDigitos(0));
+console.log(sumar(7, 9));
