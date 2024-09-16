28b2ad301494c60e79acc39141f8511dc32973d9
JuanDiegoLopezMorales <lopezjuandiego411@gmail.com>
2024-09-16 16:40:26 -0500

    Update Ejercicio1.js

--- taller JS/Ejercicio1.js
+++ taller JS/Ejercicio1.js
@@ -1,20 +1,5 @@
-const calcularFactorial = num => {
-    // Verificar si el número es positivo
-    if(num < 0) {
-        return "el numero debe ser positivo.";
-    }
-    // Verificar si el número es 0
-    else if (num === 0){
-        return 1;
-    }
-    // Calcular el factorial
-    else {  
-        let factorial = 1
-        for (let i = 1 ; i <= num; i++){
-            factorial *= i
-        }
-        return factorial;
-    }
+function saludor() {
+    console.log("Hola, mundo!");
 }
 
-console.log( calcularFactorial(5))
+saludar();
