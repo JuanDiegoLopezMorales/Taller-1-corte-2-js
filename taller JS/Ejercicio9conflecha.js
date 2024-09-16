47ffb3cbc9ef20858a886953e582c83c4b8433b5
JuanDiegoLopezMorales <lopezjuandiego411@gmail.com>
2024-09-16 17:09:54 -0500

    Update and rename Ejercicio10.js to Ejercicio9conflecha.js

---
+++ taller JS/Ejercicio9conflecha.js
@@ -0,0 +1,10 @@
+const generarSaludo = (nombre, idioma = "es") => {
+    if (idioma === "es") {
+        return "Hola, " + nombre;
+    } else if (idioma === "en") {
+        return "Hello, " + nombre;
+    }
+    return "Idioma no soportado";
+}
+console.log(generarSaludo("Santiago", "es"));
+console.log(generarSaludo("Andrea", "en"));
