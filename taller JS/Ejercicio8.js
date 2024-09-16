c0766c140aa376b2dd98a670a1be362df6a17e87
JuanDiegoLopezMorales <lopezjuandiego411@gmail.com>
2024-09-16 17:03:46 -0500

    Update Ejercicio8.js

--- taller JS/Ejercicio8.js
+++ taller JS/Ejercicio8.js
@@ -1,30 +1,4 @@
-const calcularPotencia = (base, exponente) => {
-    // Verificar si los números son enteros
-    if (!Number.isInteger(base) || !Number.isInteger(exponente)){
-        return"Ambos números deben ser enteros. ";
-    }
-
-    // Manejar el caso cuando el exponente es 0
-    if(exponente === 0){
-        return 1;
-    }
-
-    // Manejar el caso cuando la base es 0 y el exponente es negativo
-    if (base === 0 && exponente < 0){
-        return "No se puede realizar la operación.";
-    }
-
-    let resultado = 1;
-
-    // Calcular la potencia utilizando un ciclo
-    for (let i = 0; i < Math.abs(exponente); i++){
-        resultado *= base;
-    }
-
-    // Devolver el resultado inverso si el exponente es negativo
-    return exponente < 0 ? 1/resultado : resultado;
+function verificarPar(num) {
+    return num % 2 === 0;
 }
-console.log(calcularPotencia(2, 3));
-console.log(calcularPotencia(5, -2));
-console.log(calcularPotencia(0, -5));
-console.log(calcularPotencia(3.5, 2));
+console.log(verificarPar(14));
