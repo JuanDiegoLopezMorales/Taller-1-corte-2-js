49512b404098730bf7e130a43a15ad2af643ed49
JuanDiegoLopezMorales <lopezjuandiego411@gmail.com>
2024-09-16 17:00:12 -0500

    Update Ejercicio7.js

--- taller JS/Ejercicio7.js
+++ taller JS/Ejercicio7.js
@@ -1,23 +1,4 @@
-const contarVocales = str => {
-    // Convertir la cadena a minúsculas para facilitar la comparación
-str = str.toLowerCase();
-
-let vocales = 0;
-
-// Iterar sobre carácter de la cadena
-for (let i = 0; i < str.length; i++){
-    const char = str[i];
-
-    // Verofocar si el carácter es  una vocal
-    if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u'){
-        vocales++;
-    }
+function areaRectangulo(base, altura) {
+    return base * altura;
 }
-
-// Devolver el recuento de vocales
-return vocales;
-}
-console.log(contarVocales( "Hola"))
-console.log(contarVocales( "JavaScript"))
-console.log(contarVocales( "xyz"))
-console.log(contarVocales( "AeIoU"))
+console.log(areaRectangulo(10, 8));
